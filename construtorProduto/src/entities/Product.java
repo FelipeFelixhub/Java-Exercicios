89fea81469bc1ef0042f820a82efff410d2efe8f
Felipe <felipe.dfelix@gmail.com>
2023-02-24 14:22:47 -0300

    Exercicio com construtor

---
+++ construtorProduto/src/entities/Product.java
@@ -0,0 +1,38 @@
+package entities;
+
+public class Product {
+
+	public String name;
+	public double price;
+	public int quantity;
+	
+	public Product(String name, double price, int quantity) { //construtor para o produto
+		this.name = name; //This faz referencia para o proprio objeto (ref ao atributo do obj)
+		this.price = price;
+		this.quantity = quantity;
+	}
+	
+
+	public double totalValueInStock() {
+		return price * quantity;
+	}
+
+	public void addProducts(int quantity) {
+		this.quantity += quantity;
+	}
+
+	public void removeProducts(int quantity) {
+		this.quantity -= quantity;
+	}
+
+	public String toString() {
+		return name +
+				", $ " 
+				+ String.format("%.2f", price) 
+				+ ", " 
+				+ quantity 
+				+ " units, Total: $ "
+				+ String.format("%.2f", totalValueInStock());
+	}
+
+}
