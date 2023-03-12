4252bb34d83d69bd96aedbf0e50fd002406a75c3
Felipe <felipe.dfelix@gmail.com>
2023-03-11 22:15:03 -0300

    Exemplos com matriz

---
+++ matriz2/src/application/Program.java
@@ -0,0 +1,37 @@
+package application;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		int [][] mat = new int[x][y];
+		
+		for (int i = 0; i < mat.length; i++) {
+			for (int j = 0; j < mat[i].length; j++) {
+				mat[i][j] = sc.nextInt();
+				
+			}			
+		}
+		
+		for (int i = 0; i < mat.length; i++) {
+			for (int j = 0; j < mat[i].length; j++) {
+				System.out.print(mat[i][j] + " ");
+				
+			}
+			System.out.println();
+			
+		}
+		
+		
+		
+		
+		sc.close();
+
+	}
+
+}
