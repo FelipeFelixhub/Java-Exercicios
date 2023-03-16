c76fd2bffc53295805ce09aad5234aaffcf4ee80
Felipe <felipe.dfelix@gmail.com>
2023-03-16 16:26:45 -0300

    Downcasting and Upcasting

--- heranca/src/entities/BussinesAccount.java
+++ heranca/src/entities/BussinesAccount.java
@@ -1,5 +1,30 @@
 package entities;
 
-public class BussinesAccount {
+public class BussinesAccount extends Account { // com o extends a classe BA herdara todos os dados e comportamentos da classe Account 
 
+	private Double LoanLimit;
+	
+	public BussinesAccount() {
+		super();
+		
+	}
+
+	public BussinesAccount(Integer number, String holder, Double balance, Double loanLimit) {
+		super(number, holder, balance); //Construtor da super classe Account
+		LoanLimit = loanLimit;
+	}
+
+	public Double getLoanLimit() {
+		return LoanLimit;
+	}
+
+	public void setLoanLimit(Double loanLimit) {
+		LoanLimit = loanLimit;
+	}
+	
+	public void loan(double amount) {
+		if(amount <= LoanLimit) {
+			balance += amount - 10.0;
+			}
+		}
 }
---
+++ heranca/src/entities/SavingsAccount.java
@@ -0,0 +1,27 @@
+package entities;
+
+public class SavingsAccount extends Account {
+
+	private Double interestRate;
+	
+	public SavingsAccount() {
+		super();
+	}
+
+	public SavingsAccount(Integer number, String holder, Double balance, Double interstRate) {
+		super(number, holder, balance);
+		this.interestRate = interstRate;
+	}
+
+	public Double getInterestRate() {
+		return interestRate;
+	}
+
+	public void setInterestRate(Double interestRate) {
+		this.interestRate = interestRate;
+	}
+	
+	public void updateBlance() {
+		balance += balance * interestRate;
+	}
+}
