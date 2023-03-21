94a33debfb8f6329e9479b9896ae09005f5c7fe3
Felipe <felipe.dfelix@gmail.com>
2023-03-21 16:22:48 -0300

    Utilizando @Override e super

---
+++ herancaOver/src/entities/BussinesAccount.java
@@ -0,0 +1,36 @@
+package entities;
+
+public class BussinesAccount extends Account { // com o extends a classe BA herdara todos os dados e comportamentos da classe Account 
+
+	private Double LoanLimit;
+	
+	public BussinesAccount() {
+		super();
+		
+	}
+
+	public BussinesAccount(Integer number, String holder, Double balance, Double loanLimit) {
+		super(number, holder, balance); //Construtor da super classe Account
+		LoanLimit = loanLimit;
+	}
+
+	public Double getLoanLimit() {
+		return LoanLimit;
+	}
+
+	public void setLoanLimit(Double loanLimit) {
+		LoanLimit = loanLimit;
+	}
+	
+	public void loan(double amount) {
+		if(amount <= LoanLimit) {
+			balance += amount - 10.0;
+			}
+		}
+	
+	@Override
+	public void withdraw(double amount) {
+		super.withdraw(amount);
+		balance -= 2.0;
+	}
+}
