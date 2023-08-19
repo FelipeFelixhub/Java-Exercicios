2b6ece332fe296a0c15f3739bab77e3e217e28ac
Felipe Felix <felipe.dfelix@gmail.com>
2023-08-19 16:37:09 -0300

    feat: Adding Seller entity Class

--- DAO/java-dao-jdbc/src/application/Program.java
+++ DAO/java-dao-jdbc/src/application/Program.java
@@ -1,13 +1,19 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1, "Games");
-		System.out.println(obj);
+			
+		Seller seller = new Seller(10, "Felipe", "felipe@felipe.com", new Date(), 4000.00, obj);
+		
+		System.out.println(seller);
 
 	}
 
