34b1d98b0370f0c57510c7309e80850ad058ef3c
Felipe Felix <felipe.dfelix@gmail.com>
2023-08-19 18:39:28 -0300

    feat:Adding findById implementation

--- DAO/java-dao-jdbc/src/application/Program.java
+++ DAO/java-dao-jdbc/src/application/Program.java
@@ -1,21 +1,18 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "Games");
-			
-		Seller seller = new Seller(10, "Felipe", "felipe@felipe.com", new Date(), 4000.00, obj);
+		
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
 
