c395b7a5451b48aef87118227686f407f02a6e5c
Felipe Felix <felipe.dfelix@gmail.com>
2023-08-26 21:53:08 -0300

    feat: User entity and resource

---
+++ Spring boot/web-api/src/main/java/br/com/webapi/resources/UserResource.java
@@ -0,0 +1,20 @@
+package br.com.webapi.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.webapi.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Felipe", "felipe@felipe.com","111111111", "1234");
+		return ResponseEntity.ok().body(u);
+	}
+
+}
