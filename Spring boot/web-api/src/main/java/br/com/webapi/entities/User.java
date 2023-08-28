34223559ef69b473b169a9fc885945ad7298c5b7
Felipe Felix <felipe.dfelix@gmail.com>
2023-08-28 15:06:41 -0300

    feat: Order

--- Spring boot/web-api/src/main/java/br/com/webapi/entities/User.java
+++ Spring boot/web-api/src/main/java/br/com/webapi/entities/User.java
@@ -1,12 +1,15 @@
 package br.com.webapi.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -21,6 +24,12 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+	
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>(); //Associacao 1 client para N pedidos
+	
+	
 	
 	public User() {
 		
@@ -79,6 +88,10 @@ public class User implements Serializable {
 	public int hashCode() {
 		return Objects.hash(id);
 	}
+	
+	public List<Order> getOrders() {
+		return orders;
+	}
 
 	@Override
 	public boolean equals(Object obj) {
@@ -91,6 +104,8 @@ public class User implements Serializable {
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
+
+	
 	
 	
 }
